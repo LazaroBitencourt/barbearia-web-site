80c2d0e15515cf279361b07f1729bf232a9985b5
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2025-07-13 14:28:02 -0300

    Primeiro commit

---
+++ src/main/java/com/github/LazaroBitencourt/barbearia_web_site/BarbeariaWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.LazaroBitencourt.barbearia_web_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaWebSiteApplication.class, args);
+	}
+
+}
