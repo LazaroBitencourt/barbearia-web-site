0a8aec83e82b9ed70f2fedf8828a22e6a0e32da8
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2025-07-13 17:43:40 -0300

    cria classe UsuarioInfraRepository na camada infra

---
+++ src/main/java/com/github/LazaroBitencourt/barbearia_web_site/usuario/infra/UsuarioInfraRepository.java
@@ -0,0 +1,11 @@
+package com.github.LazaroBitencourt.barbearia_web_site.usuario.infra;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Log4j2
+@RequiredArgsConstructor
+public class UsuarioInfraRepository {
+}
